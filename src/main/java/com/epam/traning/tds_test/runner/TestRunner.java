a5c239d44ce859f2c8ea82b96a5f1c9bdd119c32
Aliksei <kikit33@hotmail.com>
2015-04-16 13:00:00 +0300

    Merge branch 'master' of D:\git_local\tds_test with conflicts.

--- src/main/java/com/epam/traning/tds_test/runner/TestRunner.java
+++ src/main/java/com/epam/traning/tds_test/runner/TestRunner.java
@@ -1,48 +1,32 @@
 package com.epam.traning.tds_test.runner;
 
-import org.apache.commons.cli.ParseException;
-import org.apache.log4j.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.clioption.CliParser;
-import com.epam.traning.tds_test.runner.cli.options.DriverTypeOption;
-import com.epam.traning.tds_test.runner.cli.options.SitesOption;
+import com.morning.common.runner.cli.options.DriverTypeOption;
 import com.runner.Runner;
-import com.selenium.driver.DriverManager;
 
 public class TestRunner extends Runner{
 
-	public TestRunner(String[] args) {
-		super(args);
-		
-	}
-
-	private static Logger LOGGER = Logger.getLogger(CliParser.class);
-	
 	/**
 	 * @param args
-	 * @throws ParseException 
 	 */
-	public static void main(String[] args) {
-		try {
-			Runner tr = new TestRunner(args);
-			tr.run();
-		} catch (Exception e) {
-			LOGGER.fatal(e.getMessage(), e);
-		} finally {
-			DriverManager.quitAll();
-
-			int exitCode = BuildResult.getExitResult();
-			LOGGER.info("Exit with code : " + exitCode);
-			System.exit(exitCode);
-		}
+	
+	public TestRunner(String[] args) {
+		super(args);
 	}
 	
 	public void addCommandLineOptions() {
-		super.addCommandLineOptions();
-		CliParser.getCmdLineOptions().add(new SitesOption());
 		CliParser.getCmdLineOptions().add(new DriverTypeOption());
 	}
 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
 
 
 }
